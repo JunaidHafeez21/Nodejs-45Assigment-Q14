d76713c39c544faa7e7f2c92fdc2ab6a21b0abf2
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-17 19:41:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+// Q14: Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let guestArr = ["junaid", "Ahmer", "kamran", "MISS NIDA"];
+guestArr.map((item) => (console.log(`Dear ${item}, you are invited to a dinner`)));
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Q14: Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+
+
+let guestArr: string[] = ["junaid", "Ahmer", "kamran", "MISS NIDA"];
+
+guestArr.map((item) => (
+console.log(`Dear ${item}, you are invited to a dinner`)
+));
